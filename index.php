dd656fb671ae3d72cb82a587caf827e7ad8e9e6d
epixelwebdev <leandrogrotnig11@gmail.com>
2016-09-04 01:11:56 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Less CSS | Pruebas</title>
+
+    <link rel="stylesheet/less" type="text/css" href="main.less">
+    <script type="text/javascript" src="less.js"></script>
+  </head>
+  <body>
+    <section>
+      <h1 class="etiqueta">Titulo Nº1 <span class="forma"></span></h1>
+      <h4>Autor: Gabriel</h4>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+        do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+        culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <h6>Estoy probando Less CSS.</h6>
+    </section>
+    <section>
+      <h1 class="etiqueta">Titulo Nº2 <span class="forma"></span></h1>
+      <h4>Autor: Gabriel</h4>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+        do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+        culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <h6>Estoy probando Less CSS.</h6>
+    </section>
+    <section>
+      <h1 class="etiqueta">Titulo Nº3 <span class="forma"></span></h1>
+      <h4>Autor: Gabriel</h4>
+      <p>
+        Nada nuevo...
+      </p>
+      <h6>Estoy probando Less CSS.</h6>
+    </section>
+    <section>
+      <h1 class="etiqueta">Titulo Nº4 <span class="forma"></span></h1>
+      <h4>Autor: Gabriel</h4>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
+        do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+        culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <h6>Estoy probando Less CSS.</h6>
+    </section>
+  </body>
+</html>
